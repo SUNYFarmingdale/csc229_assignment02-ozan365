3f2239ac9b7bd5baf48947c745287877534eab81
Ozan Demir <ozan2005@icloud.com>
2024-02-10 21:38:33 -0500

    Completed ToDo's: Created constructor for Student, getters and setters for Address and GPA, and toString. Demonstrates functionality in DriverClass.

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -14,6 +14,8 @@ public class DriverClass {
 		//std1 initialized with name "James", age (typecast to short) 20, GPA of 3.5, and address "123 Main Street
 		Student std1= new Student("James", (short)20,3.5, "123 Main Street");
 
+		//prompt user for input
+		System.out.println("Enter new GPA: ");
 		//set GPA to double from user input
 		std1.setGPA(scan.nextDouble());
 
